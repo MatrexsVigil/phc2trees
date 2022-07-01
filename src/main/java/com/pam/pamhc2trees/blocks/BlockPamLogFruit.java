a1c99ce6f10d7ff7564b650b98844a86b1e94484
CWDogflip <cwdogflip@gmail.com>
2022-07-01 10:39:16 -0700

    Updated fruit blocks.

--- src/main/java/com/pam/pamhc2trees/blocks/BlockPamLogFruit.java
+++ src/main/java/com/pam/pamhc2trees/blocks/BlockPamLogFruit.java
@@ -2,28 +2,27 @@ package com.pam.pamhc2trees.blocks;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.IGrowable;
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
 
-public class BlockPamLogFruit extends Block implements IGrowable {
+public class BlockPamLogFruit extends Block implements BonemealableBlock {
 	private String name;
-	public static final IntegerProperty AGE = BlockStateProperties.AGE_0_7;
+	public static final IntegerProperty AGE = BlockStateProperties.AGE_7;
 	public static boolean fruitRemoval = false;
 	public BlockPamLogFruit(Block.Properties p_i49971_1_, String name) {
 		super(p_i49971_1_);
 		this.name = name;
-		this.setDefaultState(this.stateContainer.getBaseState().with(this.getAgeProperty(), Integer.valueOf(0)));	
+		this.registerDefaultState(this.getStateDefinition().any().setValue(this.getAgeProperty(), Integer.valueOf(0)));	
 	}
 	
 	public IntegerProperty getAgeProperty() {
@@ -35,57 +34,57 @@ public class BlockPamLogFruit extends Block implements IGrowable {
 	   }
 
 	   protected int getAge(BlockState state) {
-	      return state.get(this.getAgeProperty());
+	      return state.getValue(this.getAgeProperty());
 	   }
 	   
 	   public BlockState withAge(int age) {
-		      return this.getDefaultState().with(this.getAgeProperty(), Integer.valueOf(age));
+		      return this.defaultBlockState().setValue(this.getAgeProperty(), Integer.valueOf(age));
 		   }
 
 	   public boolean isMaxAge(BlockState state) {
-	      return state.get(this.getAgeProperty()) >= this.getMaxAge();
+	      return state.getValue(this.getAgeProperty()) >= this.getMaxAge();
 	   }
 	
 	@SuppressWarnings("deprecation")
-	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
-		if (!state.isValidPosition(worldIn, pos)) {
+	public void tick(BlockState state, ServerLevel worldIn, BlockPos pos, Random random) {
+		if (!state.canSurvive(worldIn, pos)) {
 	         worldIn.destroyBlock(pos, true);
 	      }
 		super.tick(state, worldIn, pos, random);
-		int i = state.get(AGE);
-		if (i < 7 && random.nextInt(5) == 0 && worldIn.getLightSubtracted(pos.up(), 0) >= 9) {
-			worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i + 1)), 2);
+		int i = state.getValue(AGE);
+		if (i < 7 && random.nextInt(5) == 0 && worldIn.getRawBrightness(pos.above(), 0) >= 9) {
+			worldIn.setBlock(pos, state.setValue(AGE, Integer.valueOf(i + 1)), 2);
 		}
 		
 
 	}
 
-	   protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+	   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 	      builder.add(AGE);
 	   }
 
 	@Override
-	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
-		return state.get(AGE) < 7;
+	public boolean isValidBonemealTarget(BlockGetter worldIn, BlockPos pos, BlockState state, boolean isClient) {
+		return state.getValue(AGE) < 7;
 	}
 
 	@Override
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+	public boolean isBonemealSuccess(Level worldIn, Random rand, BlockPos pos, BlockState state) {
 		return true;
 	}
 	
-	protected int getBonemealAgeIncrease(World worldIn) {
-	      return MathHelper.nextInt(worldIn.rand, 2, 5);
+	protected int getBonemealAgeIncrease(Level worldIn) {
+	      return Mth.nextInt(worldIn.random, 2, 5);
 	   }
 
-	public void growFruit(ServerWorld worldIn, Random rand, BlockPos pos, BlockState state) {
+	public void growFruit(ServerLevel worldIn, Random rand, BlockPos pos, BlockState state) {
 		int i = this.getAge(state) + this.getBonemealAgeIncrease(worldIn);
 	      int j = this.getMaxAge();
 	      if (i > j) {
 	         i = j;
 	      }
 
-	      worldIn.setBlockState(pos, this.withAge(i), 2);
+	      worldIn.setBlock(pos, this.withAge(i), 2);
 	}
 	
 
@@ -94,14 +93,14 @@ public class BlockPamLogFruit extends Block implements IGrowable {
 
 
 	@Override
-	public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos pos) {
+	public boolean canSurvive(BlockState state, LevelReader world, BlockPos pos) {
 
 		return true;
 		
 	}
 
 	@Override
-	public void grow(ServerWorld p_225535_1_, Random p_225535_2_, BlockPos p_225535_3_, BlockState p_225535_4_) {
+	public void performBonemeal(ServerLevel p_225535_1_, Random p_225535_2_, BlockPos p_225535_3_, BlockState p_225535_4_) {
 		this.growFruit(p_225535_1_, p_225535_2_, p_225535_3_, p_225535_4_);
 		
 	}
